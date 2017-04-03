758aabe0a96810cb408d47f78e9c1c35bd71bf2b
Yun Xu <yunx@zillowgroup.com>
2017-04-02 22:43:05 -0700

    add interface for carbon-index

---
+++ carbon_index/index.py
@@ -0,0 +1,31 @@
+
+class TrieNode:
+
+	def __init__(self, name, is_leaf=True):
+		self.is_leaf = is_leaf
+		self.name = name
+		self.children = dict()
+
+	def delete(self, child_name):
+
+
+	def append(self, child_name):
+
+
+
+class CarbonIndex:
+
+	def __init__(self):
+
+
+	def insert(self, metric):
+
+
+	def search(self, metric):
+
+
+	def prefix_search(self, prefix):
+
+
+	def delete(self, metric):
+		
---
+++ carbon_index/node.py
@@ -0,0 +1,13 @@
+
+class TrieNode:
+
+	def __init__(self, name, is_leaf=True):
+		self.is_leaf = is_leaf
+		self.name = name
+		self.children = dict()
+
+	def delete(self, child_name):
+
+
+	def append(self, child_node):
+		
