315eb0df6c9eef299e81901ff310bb5baabe3ab9
Yun Xu <yunx@zillowgroup.com>
2017-12-27 12:11:37 -0800

    bumps up version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = ["lru-dict"]
 tests_require = []
 
 setup(name='carbon-index',
-      version='0.1.4',
+      version='0.1.5',
       description='',
       long_description=open(README_PATH).read(),
       author='Yun Xu',
