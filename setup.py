171c3a883d420606141cbc1221d6b04806d1ff3c
Yun Xu <yunx@zillowgroup.com>
2018-05-25 17:09:27 -0700

    bump up version 0.1.11

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ ext_modules = [
 ]
 
 setup(name='carbon-index',
-      version='0.1.10',
+      version='0.1.11',
       description='',
       long_description=open(README_PATH).read(),
       author='Yun Xu',
